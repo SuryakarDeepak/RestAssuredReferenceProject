e897c2949a77af893cf5b48e40b11f59055f6434
Manu <Manu@DESKTOP-J200OE5>
2023-05-26 11:48:12 +0530

    RestAssuredReferenceProject

---
+++ src/restAssuredReference/delete_reference4.java
@@ -0,0 +1,20 @@
+package restAssuredReference;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import org.testng.Assert;
+
+public class delete_reference4 {
+
+	public static void main(String[] args) {
+	RestAssured.baseURI="https://reqres.in/";
+	
+	int statuscode = given().header("content-Type","application/java").when().delete("/api/users/2").then().extract().statusCode();
+    Assert.assertEquals(statuscode,204);
+    
+    System.out.println("status Code :"+ statuscode);
+    System.out.println("DELETE METHOD SUCCESSFULLY VALIDATED...!");
+    
+	}
+
+}
---
+++ src/restAssuredReference/post_reference3.java
@@ -0,0 +1,51 @@
+package restAssuredReference;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import static io.restassured.RestAssured.given;
+import org.testng.Assert;
+import java.time.LocalDateTime;
+public class post_reference3 {
+
+	public static void main(String[] args) {
+		String BaseURI="https://reqres.in/";
+		String RequestBody = "{\r\n"
+				+ "    \"name\": \"morpheus\",\r\n"
+				+ "    \"job\": \"leader\"\r\n"
+				+ "}";
+		LocalDateTime Date = LocalDateTime.now();
+		String expectDate = Date.toString().substring(0,11);
+		
+		JsonPath jsprerequest = new JsonPath(RequestBody);
+		String req_name = jsprerequest.getString("name");
+		String req_job = jsprerequest.getString("job");
+		String req_email = jsprerequest.getString("email");
+		
+		RestAssured.baseURI= BaseURI;
+		
+		int statuscode = given().header("Content-Type","application/json").body(RequestBody).when().post("/api/users").then().extract().statusCode();
+		String responseBody = given().header("Content-Type","application/json").body(RequestBody).when().post("/api/users").then().extract().response().asString();
+		System.out.println("status Code :"+ statuscode);
+		System.out.println("status Body :"+ responseBody);
+		
+		JsonPath jsp = new JsonPath(responseBody);
+		String res_name = jsp.getString("name");
+		String res_job = jsp.getString("job");
+		String res_id= jsp.getString("id");
+		String res_email = jsp.getString("email");
+		String res_createdAt = jsp.getString("createdAt");
+		res_createdAt = res_createdAt.substring(0,11);
+		
+		Assert.assertEquals(statuscode, 201);
+		Assert.assertEquals(res_name,req_name);
+		Assert.assertEquals(res_job,req_job);
+		Assert.assertNotNull(res_id);
+		Assert.assertEquals(res_email,req_email);
+		Assert.assertEquals(res_createdAt, expectDate);
+		
+		System.out.println("Date :"+ expectDate);
+		System.out.println("POST METHOD SUCCESSFULLY VALIDATED");
+		
+	}
+
+}
